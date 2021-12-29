b9593aec65c1c3624397804766ca8e67022e8516
edbertwicohen <81284981+edbertwicohen@users.noreply.github.com>
2021-12-30 02:30:09 +0800

    Penambahan logo dan design UI konfirmasiTilang

--- lib/Pages/Navbar/ListTilang/Pembayaran/konfirmasi_tilang.dart
+++ lib/Pages/Navbar/ListTilang/Pembayaran/konfirmasi_tilang.dart
@@ -1,13 +1,164 @@
 import 'package:flutter/material.dart';
 
-class KonfirmasiTilang extends StatefulWidget {
-  @override
-  _KonfirmasiTilangState createState() => _KonfirmasiTilangState();
-}
-
-class _KonfirmasiTilangState extends State<KonfirmasiTilang> {
+class KonfirmasiTilang extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Konfirmasi Tilang')),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 16),
+                    child: Center(
+                      child: Image(
+                        image: AssetImage("assets/logo-polri.png"),
+                        height: 80,
+                        width: 80,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "KEPOLISIAN DAERAH METROJAYA",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "DIREKTORAT LALU LINTAS",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Jl. Jenderal Sudirman No. 55, Jakarta, 12190",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //Buat isi nomor, platKendaraan, dan mesinKendaraan dari db
+                    Text(
+                      "Nomor : KPO/VII/12112021",
+                      style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "Perihal: Surat Konfirmasi ETLE",
+                      style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "Tempat/Tanggal: Jakarta, 29 Agustus 2021",
+                      style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //Buat isi jenisKendaraan, perihal, dan tempat/tanggal dari db
+                    Text(
+                      "Detail Informasi",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey),
+                    ),
+                    Text(
+                      "Jenis Kendaraan: Mobil",
+                      style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "Nomor Plat Kendaraan: B2224FE",
+                      style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "Nomor Mesin Kendaraan:  MHKV3BA0538432",
+                      style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: Center(
+                  child: Image(
+                    image: AssetImage("assets/img1.png"),
+                    height: 200,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: Column(
+                  children: [
+                    MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60,
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                //Nanti dipilih berdasarkan index
+                                builder: (context) => KonfirmasiTilang()));
+                      },
+                      color: Colors.blue,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "Ya, saya sebagai pelanggar",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60,
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                //Nanti dipilih berdasarkan index
+                                builder: (context) => KonfirmasiTilang()));
+                      },
+                      color: Colors.red,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "Saya bukan sebagai pelanggar",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
