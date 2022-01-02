330dba87ada554f897d946df978843317e9d1b51
edbertwicohen <81284981+edbertwicohen@users.noreply.github.com>
2022-01-03 00:51:31 +0800

    Update ProfilePage

--- lib/Pages/Navbar/Settings/my_profile.dart
+++ lib/Pages/Navbar/Settings/my_profile.dart
@@ -1,27 +1,11 @@
-import 'package:aplikasi_tilang_training/net/firebase.dart';
-import 'package:aplikasi_tilang_training/Authentication/login.dart';
-import 'package:aplikasi_tilang_training/Authentication/otp.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class MyProfile extends StatefulWidget {
-  const MyProfile({Key key}) : super(key: key);
-
   @override
-  _RegistrationPageState createState() => _RegistrationPageState();
+  _MyProfileState createState() => _MyProfileState();
 }
 
-class _RegistrationPageState extends State<MyProfile> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController =
-      TextEditingController();
-  final TextEditingController _phoneNumberController = TextEditingController();
-
+class _MyProfileState extends State<MyProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,166 +14,38 @@ class _RegistrationPageState extends State<MyProfile> {
         title: Text("Profile", style: TextStyle(color: Colors.black)),
         backgroundColor: Color.fromRGBO(245, 245, 245, 1),
       ),
-      resizeToAvoidBottomInset: true,
-      backgroundColor: Colors.white,
-      body: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 40),
-            // height: MediaQuery.of(context).size.height - 50,
-            width: double.infinity,
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(right: 20, top: 50, bottom: 20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [],
-                    ),
-                  ),
-                  Column(
-                    children: <Widget>[
-                      inputFile(
-                          label: "Nama Lengkap",
-                          controller: _usernameController),
-                      inputFile(label: "Email", controller: _emailController),
-                      inputFile(
-                          label: "Password",
-                          controller: _passwordController,
-                          obscureText: true),
-                      inputPhoneNum(
-                          label: "Nomor Handphone",
-                          controller: _phoneNumberController)
-                    ],
-                  ),
-                  // Container(
-                  //   padding: EdgeInsets.only(top: 3, left: 3),
-                  //   decoration: BoxDecoration(
-                  //     borderRadius: BorderRadius.circular(50),
-                  //   ),
-                  //   child: MaterialButton(
-                  //     minWidth: double.infinity,
-                  //     height: 60,
-                  //     onPressed: () async {
-                  //       if (_formKey.currentState.validate()) {
-                  //         if (_passwordController.text.trim() ==
-                  //             _confirmPasswordController.text.trim()) {
-                  //           try {
-                  //             await FirebaseAuth.instance
-                  //                 .createUserWithEmailAndPassword(
-                  //                     email: _emailController.text,
-                  //                     password: _passwordController.text);
-                  //             setState(() {
-                  //               Fluttertoast.showToast(
-                  //                   msg: "succesffuly signed up!");
-                  //               Navigator.pushReplacement(
-                  //                 context,
-                  //                 MaterialPageRoute(
-                  //                     builder: (context) => OTPScreen(
-                  //                         _phoneNumberController.text,
-                  //                         _usernameController.text)),
-                  //               );
-                  //               User updateUser =
-                  //                   FirebaseAuth.instance.currentUser;
-                  //               updateUser.updateDisplayName(
-                  //                   _usernameController.text);
-                  //               userSetup(_usernameController.text);
-                  //             });
-                  //           } catch (e) {
-                  //             Fluttertoast.showToast(msg: "Error! $e");
-                  //           }
-                  //         } else {
-                  //           Fluttertoast.showToast(
-                  //               msg: "Error! Passwords doesn't match");
-                  //         }
-                  //       }
-                  //     },
-                  //     color: Colors.blue,
-                  //     elevation: 0,
-                  //     shape: RoundedRectangleBorder(
-                  //       borderRadius: BorderRadius.circular(10),
-                  //     ),
-                  //     child: Text(
-                  //       "Daftar",
-                  //       style: TextStyle(
-                  //           fontWeight: FontWeight.w600,
-                  //           fontSize: 18,
-                  //           color: Colors.white),
-                  //     ),
-                  //   ),
-                  // ),
-                ]),
-          ),
-        ),
-      ),
+      body: Container(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            children: [
+              ListTile(
+                title: Text('Nama Lengkap',
+                    style:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+                subtitle: Text('Willy Hoanandar',
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
+              ),
+              ListTile(
+                title: Text('Email',
+                    style:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+                subtitle: Text('WillyHoanandar@gmail.com',
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
+              ),
+              ListTile(
+                title: Text('Nomor Handphone',
+                    style:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+                subtitle: Text('08123456789',
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
+              )
+            ],
+          )),
     );
   }
-}
-
-Widget inputFile({label, controller, obscureText = false, value}) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text(
-        label,
-        style: TextStyle(
-            fontSize: 14,
-            fontWeight: FontWeight.w400,
-            color: Colors.black87,
-            height: 1),
-      ),
-      Padding(
-        padding: const EdgeInsets.only(bottom: 25),
-        child: TextFormField(
-          obscureText: obscureText,
-          controller: controller,
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 0),
-          ),
-          validator: validateText,
-        ),
-      )
-    ],
-  );
-}
-
-String validateText(String formText) {
-  if (formText.isEmpty) return "Tidak boleh kosong!";
-}
-
-Widget inputPhoneNum({label, controller, obscureText = false}) {
-  return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 9.0),
-          child: Text(
-            label,
-            style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                color: Colors.black87,
-                height: 1),
-          ),
-        ),
-        SizedBox(
-          height: 60,
-          child: TextFormField(
-            controller: controller,
-            decoration: const InputDecoration(
-              fillColor: Colors.blue,
-              prefix: Text("+62 "),
-              border: UnderlineInputBorder(),
-            ),
-            validator: validateText,
-            keyboardType: TextInputType.number,
-          ),
-        )
-      ]);
 }
 
 //CODE AWAL PROFILEPAGE
